d7d40abf7421ddb64f1231ecf490f6339389d626
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-11-14 22:41:11 +0700

    fix bug trailer movie list

--- src/components/TrailerMovieList.js
+++ src/components/TrailerMovieList.js
@@ -9,21 +9,19 @@ function TrailerMovieList({ trailerMovieList }) {
             .filter((trailer) => trailer.type === "Trailer")
             .slice(0, 2)
             .map((trailer) => (
-              <>
-                <Grid key={trailer.id} sx={{ m: 1 }}>
-                  <Typography variant="subtitle1">{trailer?.name}</Typography>
-                  <Box key={trailer.id}>
-                    <iframe
-                      width="300"
-                      height="300"
-                      src={`https://www.youtube.com/embed/${trailer.key}`}
-                      frameBorder="0"
-                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                      title="Embedded youtube"
-                    />
-                  </Box>
-                </Grid>
-              </>
+              <Grid key={trailer.id} sx={{ m: 1 }}>
+                <Typography variant="subtitle1">{trailer?.name}</Typography>
+                <Box>
+                  <iframe
+                    width="300"
+                    height="300"
+                    src={`https://www.youtube.com/embed/${trailer.key}`}
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    title="Embedded youtube"
+                  />
+                </Box>
+              </Grid>
             ))
         : "No Trailer Movie Data Found."}
     </Grid>
