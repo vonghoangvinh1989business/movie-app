738baa09a3719c20c44f56d93e40f9a618d39018
Vong Hoang Vinh <vonghoangvinh1989@gmail.com>
2022-11-16 15:08:51 +0700

    update file index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const container = document.querySelector("#root");
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(<App />, document.querySelector("#root"));
